0bada7f2e45fa5039ac3fe1028fab3a586dfde02
shinshARK <137866950+shinshARK@users.noreply.github.com>
2024-02-25 08:37:17 +0700

    refactor(validation): decoupled form validation to be a separate package for reusability

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,18 +5,17 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"strings"
-	"unicode/utf8"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/shinshARK/snippetbox/internal/models"
+	"github.com/shinshARK/snippetbox/internal/validator"
 )
 
 type snippetCreateForm struct {
-	Title       string
-	Content     string
-	Expires     int
-	FieldErrors map[string]string
+	Title   string
+	Content string
+	Expires int
+	validator.Validator
 }
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -85,27 +84,18 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	}
 
 	form := snippetCreateForm{
-		Title: r.PostForm.Get("title"),
+		Title:   r.PostForm.Get("title"),
 		Content: r.PostForm.Get("content"),
 		Expires: expires,
-		FieldErrors: map[string]string{},
 	}
 
-	if strings.TrimSpace(form.Title) == "" {
-		form.FieldErrors["title"] = "This field cannot be blank"
-	} else if utf8.RuneCountInString(form.Title) > 100 {
-		form.FieldErrors["title"] = "This field cannot be more than 100 characters long"
-	}
+	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters long")
+	form.CheckField(validator.NotBlank(form.Content), "title", "This field cannot be blank")
+	form.CheckField(validator.PermittedIntValue(form.Expires, 1, 7, 365), "expires", "This field must equal to 1, 7 or 365")
 
-	if strings.TrimSpace(form.Content) == "" {
-		form.FieldErrors["content"] = "This field cannot be blank"
-	}
-
-	if form.Expires != 1 && form.Expires != 7 && form.Expires != 365 {
-		form.FieldErrors["expires"] = "This field must equal to 1, 7 or 365"
-	}
-
-	if len(form.FieldErrors) > 0 {
+	// if any invalid checked
+	if !form.Valid() {
 		// fmt.Fprint(w, fieldErrors)
 		data := app.newTemplateData(r)
 		data.Form = form
